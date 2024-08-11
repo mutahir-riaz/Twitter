a1e91c6149293f8fd6700344c843f3359b5d64d3
mutahir-riaz <mutahirriaz4@gmail.com>
2024-08-11 10:26:01 +0500

    Uploading via termial -Syed Mutahir

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.json());
 const PORT = 5000 ||process.env.PORT;
 
 // MongoDB URI
-const MONGODB_URI = 'mongodb+srv://syedmutahir908:Mongoauth123@cluster0.da7uyp9.mongodb.net/';
+const MONGODB_URI = process.env.URI_MONGO;
 
 // Connect to MongoDB
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -49,64 +49,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Create HTTP server and initialize Socket.io
-// const server = http.createServer(app);
-// const io = new Server(server, {
-//   cors: {
-//     origin: "http://localhost:3000",
-//     methods: ["GET", "POST"]
-//   }
-// });
 
-
-let users = {}; // Object to store username: socket.id pairs
-
-// // Handle Socket.io connections
-// io.on('connection', (socket) => {
-//     console.log('A user connected');
-  
-//     // When a user sets their username
-//     socket.on('set username', (username) => {
-//       console.log(`${username} is connected`);
-//       users[username] = socket.id; // Store the username and socket.id pair
-//       socket.username = username;
-//     });
-  
-//     // When a user sends a private message
-//     socket.on('private message', ({ recipient, message }) => {
-//       const recipientSocketId = users[recipient];
-//       if (recipientSocketId) {
-//         io.to(recipientSocketId).emit('private message', {
-//           sender: socket.username,
-//           message: message,
-//         });
-//       }
-//     });
-  
-//     // Handle typing notifications
-//     socket.on('typing', (recipient) => {
-//       const recipientSocketId = users[recipient];
-//       if (recipientSocketId) {
-//         io.to(recipientSocketId).emit('typing', `${socket.username} is typing...`);
-//       }
-//     });
-  
-//     socket.on('stop typing', (recipient) => {
-//       const recipientSocketId = users[recipient];
-//       if (recipientSocketId) {
-//         io.to(recipientSocketId).emit('stop typing');
-//       }
-//     });
-  
-//     // When a user disconnects
-//     socket.on('disconnect', () => {
-//       if (socket.username) {
-//         console.log(`${socket.username} disconnected`);
-//         delete users[socket.username]; // Remove the user from the list
-//       }
-//     });
-//   });
-// // Start the server
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
